6c729b59d309c01283723988421a2ace15a2c5fb
CoreSRT <botanicrus@gmail.com>
2024-03-20 20:16:14 +0300

    some working directory magic added requirements.txt added .gitignore added

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from pikepdf import Pdf
+import os
+from icecream import ic
 
+
+filename = input('Введите название файла: ')
+ic(filename)
+
+savedir = os.getcwd()
+if not filename.endswith('.pdf'):
+    filename += '.pdf'
+ic(filename)
 startfrom = int(input('Введите номер страницы, где расположен первый билет: '))
 
 with Pdf.open('order.pdf') as pdf:
 
+    if not os.path.exists('output'):
+        os.makedirs('output')
+    os.chdir('output')
+    ic(os.getcwd())
     for n, page in enumerate(pdf.pages, start=1):
         if n >= startfrom:
             sep_page = Pdf.new()
             sep_page.pages.append(page)
-            sep_page.save(f'Билет {n - (startfrom - 1):02d}.pdf')
+            # sep_page.save(f'Билет {n - (startfrom - 1)}.pdf')
+            ic(f'Я создаль {n - (startfrom - 1)}.pdf')
+os.chdir(savedir)
