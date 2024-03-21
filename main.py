951566ddab5a621c078c237eef2ed8c7d4a307f5
Dmitry <botanicrus@gmail.com>
2024-03-21 11:13:58 +0300

    работа над автоматическим поиском файла в директории

--- main.py
+++ main.py
@@ -3,25 +3,57 @@ import os
 from icecream import ic
 
 
-filename = input('Введите название файла: ')
-ic(filename)
+def find_pdf():
+	file_name = ''
+	pdf_count = 0
+	for file in os.listdir():
+		if os.path.isfile(file) and file.endswith(".pdf"):
+			file_name = file
+			pdf_count += 1
+		if pdf_count == 1:
+			return file_name
+		else:
+			print(f'Внимание! В директории должен быть только один файл pdf!')
+			break
+	return None
 
-savedir = os.getcwd()
-if not filename.endswith('.pdf'):
-    filename += '.pdf'
-ic(filename)
-startfrom = int(input('Введите номер страницы, где расположен первый билет: '))
 
-with Pdf.open('order.pdf') as pdf:
+# TODO Доделать функцию, которая будет проверять наличие файла в папке
+def check_file(filename: str):
+	try:
+		if os.path.exists(filename) and os.path.isfile(filename):
+			return True
+		else:
+			ic(f' Тут моя хваленая ошибка{warnings['file_not_found']}')
+	except Exception as e:
+		ic(f'Ошибка{e}')
 
-    if not os.path.exists('output'):
-        os.makedirs('output')
-    os.chdir('output')
-    ic(os.getcwd())
-    for n, page in enumerate(pdf.pages, start=1):
-        if n >= startfrom:
-            sep_page = Pdf.new()
-            sep_page.pages.append(page)
-            # sep_page.save(f'Билет {n - (startfrom - 1)}.pdf')
-            ic(f'Я создаль {n - (startfrom - 1)}.pdf')
-os.chdir(savedir)
+
+def main():
+	savedir: str = os.getcwd()
+	ic(os.listdir())
+	filename: str = ''
+	if find_pdf():
+		filename: str = find_pdf()
+	ic(filename)
+
+	startfrom: int = int(input('Введите номер страницы, где расположен первый билет: '))
+
+	with Pdf.open(filename) as pdf:
+
+		if not os.path.exists('output'):
+			os.makedirs('output')
+		os.chdir('output')
+		ic(os.getcwd())
+		for n, page in enumerate(pdf.pages, start=1):
+			if n >= startfrom:
+				sep_page: Pdf = Pdf.new()
+				sep_page.pages.append(page)
+				# sep_page.save(f'Билет {n - (startfrom - 1)}.pdf')
+				ic(f'Я создаль {n - (startfrom - 1)}.pdf')
+	os.chdir(savedir)
+	ic(os.getcwd())
+
+
+if __name__ == '__main__':
+	main()
